445f75b8acffff07a14796c38ee3786ad89120ec
Jace Browning <jacebrowning@gmail.com>
2025-04-22 16:40:30 -0400

    Log branch issues

--- tab/projects/models.py
+++ tab/projects/models.py
@@ -254,19 +254,21 @@ class Test(models.Model):
                 self.update_average_duration(),
             ]
         )
-        if self.pk:
-            log.critical(f"TODO: {self.project.default_branch=}")
-            self.last_result = (
-                self.results.filter(branch=self.project.default_branch)
-                .order_by("-created_at")
-                .first()
+        if self.project.default_branch != "main":
+            log.critical(
+                f"TODO: {self=} {self.project=} {self.project.default_branch=}"
             )
-            log.critical(f"TODO: {self.last_result=}")
+        self.last_result = (
+            self.results.filter(branch=self.project.default_branch)
+            .order_by("-created_at")
+            .first()
+        )
+        # log.critical(f"TODO: {self.last_result=}")
         self.enabled = bool(
             self.last_result
             and self.last_result.status not in {Status.SKIPPED, Status.DISABLED}
         )
-        log.critical(f"TODO: {self.enabled=}")
+        # log.critical(f"TODO: {self.enabled=}")
         return updated
 
 
