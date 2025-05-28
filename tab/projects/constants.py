3f46a9290715649b49173997235a0f2d1cd83289
jacebrowning <jacebrowning@gmail.com>
2025-05-28 09:26:14 -0400

    Return to (a lower) block rate to detect new failures

--- tab/projects/constants.py
+++ tab/projects/constants.py
@@ -7,7 +7,7 @@ DEFAULT_SUITE = "default"
 ANSI_ESCAPE = re.compile(r"\x1B[@-_][0-?]*[ -/]*[@-~]")
 
 PENDING_THRESHOLD = timedelta(minutes=10)  # duration to stop waiting for new results
-NEW_FAILURE_THRESHOLD = 0.1  # minimum failure rate to consider failures expected
+NEW_FAILURE_THRESHOLD = 0.01  # minimum block rate to consider failures expected
 
 
 def get_default_branches():
