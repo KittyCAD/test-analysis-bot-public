8533fa83588f670838958cd66fc03ab79b5662f5
Jace Browning <jacebrowning@gmail.com>
2025-11-14 16:37:41 -0500

    Alert teams up to once per hour

--- tab/metrics/constants.py
+++ tab/metrics/constants.py
@@ -5,4 +5,4 @@ DELTA_THRESHOLD = 0.25  # percentage point change to alert on metrics
 ALERT_CACHE_KEY = "metrics:alert"
 ALERT_CACHE_TIMEOUT = timedelta(days=1.5).total_seconds()
 
-ALERT_LIMIT = timedelta(hours=4)  # window to limit alerts for a single team
+ALERT_LIMIT = timedelta(hours=1)  # window to limit alerts for a single team
