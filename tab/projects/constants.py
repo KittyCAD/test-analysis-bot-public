22bbbb361dc4c0c8ec60ab24352d673b0a19da8b
Jace Browning <jacebrowning@gmail.com>
2026-01-08 14:58:18 -0500

    Lower cache
    We really only need the cache to be valid for a single request.

--- tab/projects/constants.py
+++ tab/projects/constants.py
@@ -13,7 +13,7 @@ PENDING_THRESHOLD = timedelta(minutes=15)  # duration to stop expecting new resu
 FAILURE_RATE_EPSILON = 0.001  # small value to keep tests disabled for a bit longer
 
 SETUP_DURATION_CACHE_KEY = "projects:setup_duration"
-SETUP_DURATION_CACHE_TIMEOUT = timedelta(days=1).total_seconds()
+SETUP_DURATION_CACHE_TIMEOUT = timedelta(minutes=1).total_seconds()
 
 
 def get_default_branches():
