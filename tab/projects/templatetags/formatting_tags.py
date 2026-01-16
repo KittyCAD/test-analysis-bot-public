74c69ba213f13f83c3a2c16d19578fa0d0aa0e79
Jace Browning <jacebrowning@gmail.com>
2026-01-16 16:08:51 -0500

    Highlight diffs in test output (#262)

--- tab/projects/templatetags/formatting_tags.py
+++ tab/projects/templatetags/formatting_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
 
 from tab.metrics.constants import DELTA_THRESHOLD
 
@@ -11,6 +13,7 @@ register = template.Library()
 
 @register.simple_tag
 def colorize(test: Test, field_name: str) -> str:
+    """Add color and trend indicators to test attributes."""
     if not test.suite:
         return getattr(test, f"{field_name}_humanized")
 
@@ -41,4 +44,34 @@ def colorize(test: Test, field_name: str) -> str:
 
 @register.filter
 def duration(value):
+    """Format durations to show minutes and seconds."""
     return humanize_duration(value)
+
+
+@register.filter
+def highlight(value):
+    """Highlight diff lines: green for additions (+), red for deletions (-)."""
+    if not value:
+        return value
+
+    lines = value.splitlines(keepends=True)
+    result_lines = []
+
+    for line in lines:
+        has_newline = line.endswith("\n")
+        line_content = line.rstrip("\n\r")
+
+        if line_content.startswith("+"):
+            escaped = escape(line_content)
+            result_lines.append(f'<span class="text-success">{escaped}</span>')
+            if has_newline:
+                result_lines.append("\n")
+        elif line_content.startswith("-"):
+            escaped = escape(line_content)
+            result_lines.append(f'<span class="text-danger">{escaped}</span>')
+            if has_newline:
+                result_lines.append("\n")
+        else:
+            result_lines.append(escape(line))
+
+    return mark_safe("".join(result_lines))
