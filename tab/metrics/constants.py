a23b534e1f47e47eda1f7cfb8e306e4d4e04f681
Jace Browning <jacebrowning@gmail.com>
2026-01-20 09:43:35 -0500

    Wait for a higher failure rate before alerting

--- tab/metrics/constants.py
+++ tab/metrics/constants.py
@@ -1,6 +1,6 @@
 from datetime import timedelta
 
-DELTA_THRESHOLD = 0.25  # percentage point change to alert on metrics
+DELTA_THRESHOLD = 0.33  # percentage point change to alert on metrics
 
 ALERT_CACHE_KEY = "metrics:alert"
 ALERT_CACHE_TIMEOUT = timedelta(days=1.5).total_seconds()
