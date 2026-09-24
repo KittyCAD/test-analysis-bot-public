c2e80fb3a01fb649b9a4be65d98d621aa3e164d2
Jace Browning <jacebrowning@gmail.com>
2026-09-24 13:29:23 -0400

    Finalize releases sooner

--- tab/releases/constants.py
+++ tab/releases/constants.py
@@ -1,7 +1,11 @@
+from datetime import timedelta
+
 from tab.projects.constants import PENDING_THRESHOLD
 
 PLACEHOLDER_CHARACTER = "{"  # indicates a placeholder URL, e.g. "app-{slug}"
 
 CHANGE_HISTORY_LIMIT = 50  # default number of displayed change history items
 
-RESULTS_TIMEOUT = PENDING_THRESHOLD * 2  # maximum time to wait for results
+RESULTS_TIMEOUT = PENDING_THRESHOLD + timedelta(
+    minutes=5
+)  # maximum time to wait for results
